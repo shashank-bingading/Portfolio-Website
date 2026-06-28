f8c7fece3fe9b2e3f5afa661322f063b47998904
Shashank Singh <shashanksinghwork88@gmail.com>
2026-06-28 23:13:03 +0530

    Added Card Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,96 @@
-import './App.css'
-import PixelBlast from '../@/components/PixelBlast';
+import "./App.css";
+import PixelBlast from "../@/components/PixelBlast";
+import CardNav from "./components/CardNav";
+import Hero from "./components/Hero";
+
 function App() {
-  
+  const items = [
+    {
+      label: "About",
+      bgColor: "#1B1722",
+      textColor: "#fff",
+      links: [
+        {
+          label: "Company",
+          href: "/contact/email",
+          ariaLabel: "About Company",
+        },
+        {
+          label: "Careers",
+          href: "/contact/email",
+          ariaLabel: "About Careers",
+        },
+      ],
+    },
+    {
+      label: "Projects",
+      bgColor: "#2F293A",
+      textColor: "#fff",
+      links: [
+        {
+          label: "Featured",
+          href: "/contact/email",
+          ariaLabel: "Featured Projects",
+        },
+        {
+          label: "Case Studies",
+          href: "/contact/email",
+          ariaLabel: "Project Case Studies",
+        },
+      ],
+    },
+    {
+      label: "Contact",
+      bgColor: "#2F293A",
+      textColor: "#fff",
+      links: [
+        { label: "Email", href: "/contact/email", ariaLabel: "Email us" },
+        { label: "Twitter", href: "/contact/email", ariaLabel: "Twitter" },
+        { label: "LinkedIn", href: "/contact/email", ariaLabel: "LinkedIn" },
+      ],
+    },
+  ];
 
   return (
     <>
-
-<div style={{ width: '100%', height: '600px', position: 'relative' }}>
-  <PixelBlast
-    variant="square"
-    pixelSize={4}
-    color="#B497CF"
-    patternScale={2}
-    patternDensity={1}
-    pixelSizeJitter={0}
-    enableRipples
-    rippleSpeed={0.4}
-    rippleThickness={0.12}
-    rippleIntensityScale={1.5}
-    liquid={false}
-    liquidStrength={0.12}
-    liquidRadius={1.2}
-    liquidWobbleSpeed={5}
-    speed={0.5}
-    edgeFade={0.25}
-    transparent
-  />
-</div>
-     
+      <div style={{ width: "100%", height: "600px", position: "absolute" }}>
+        <PixelBlast
+          variant="square"
+          pixelSize={4}
+          color="#B497CF"
+          patternScale={2}
+          patternDensity={1}
+          pixelSizeJitter={0}
+          enableRipples
+          rippleSpeed={0.4}
+          rippleThickness={0.12}
+          rippleIntensityScale={1.5}
+          liquid={false}
+          liquidStrength={0.12}
+          liquidRadius={1.2}
+          liquidWobbleSpeed={5}
+          speed={0.5}
+          edgeFade={0.25}
+          transparent
+        />
+        </div>
+        <div>
+          <CardNav
+            logoAlt="Company Logo"
+            items={items}
+            baseColor="#fff"
+            menuColor="#000"
+            buttonBgColor="#111"
+            buttonTextColor="#fff"
+            ease="power3.out"
+          />
+        </div>
+        <div>
+          <Hero />
+        </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,13 @@
 const Hero = () => {
   return (
     <div>
-      
+      <div className={`w-50  h-50`}>
+        <img src="/1000188233.jpg" alt="Hero Image" />
+
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
